47f8799311066c7b611c3fab199f6019f6b2272e
pilarfajardo2328@gmail.com <stephen.fajardo@bentacos.com>
2019-07-02 22:53:05 +0800

    sample

---
+++ my-cv-file/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div class="container">
+        <div className="row">
+            <div className="col-md">
+              <img src="" alt="" className="img-thumbnail"></img>
+            </div>
+            <div className="col-md">
+            <h1 className="text-white-50 bg-dark">Stephen Pol Fajardo</h1>
+            <p className="col-2">To take up any challenging position when my knowledge can be utilized for the maximum benefit of the organization. Open to new ideas and an attitude as well as potential to group and implement new technologies.
+            </p>
+            </div>
+        </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
