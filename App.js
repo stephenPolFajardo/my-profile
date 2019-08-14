ffaaaad2eabd65b96c6f19d1008866d733d2df67
StephenPol <52106088+stephenPolFajardo@users.noreply.github.com>
2019-08-14 20:06:55 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import TicTacToe from "./tictactoe";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Start Here</h1>
+      <TicTacToe />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
