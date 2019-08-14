863c6bd294590a06a88921ecc8fe370116b311ce
StephenPol <52106088+stephenPolFajardo@users.noreply.github.com>
2019-08-14 20:07:44 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -15,18 +15,3 @@ function App() {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-import React, { Component } from "react";
-import TicTacToe from "./tictactoe";
-
-import "./App.css";
-
-function App() extends Component {
-  return (
-    <div className="App">
-      <h1>Start Here</h1>
-      <TicTacToe />
-    </div>
-  );
-}
-
-export default App;
